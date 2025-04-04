c8cabefaa9628f1af1e3f54f666bd54a3adf515f
nadika <nadikazavodovska@gmail.com>
2025-04-04 16:38:31 +0100

    Refactor code

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ import path from 'path';
 // Set up the Express app
 const app = express();
 // Port number for the server to listen to
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Enable CORS for all domains, allows any website to use the server
 app.use(cors());
